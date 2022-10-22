2677b2ecae274fc81c52e5b96d0c0a46650d81a6
anna <9408commit@gmail.com>
2022-10-22 22:58:16 +0900

    feat: UserSimpleOut.kt에 userOid 추가

--- api/src/main/kotlin/kr/co/apexsoft/fw/api/dto/user/UserSimpleOut.kt
+++ api/src/main/kotlin/kr/co/apexsoft/fw/api/dto/user/UserSimpleOut.kt
@@ -3,12 +3,14 @@ package kr.co.apexsoft.fw.api.dto.user
 import kr.co.apexsoft.fw.domain.model.user.User
 
 data class UserSimpleOut(
+    val userOid: Long,
     val userId: String,
     val nickName: String
 ) {
     companion object {
         fun fromEntity(e: User): UserSimpleOut {
             return UserSimpleOut(
+                userOid = e.oid!!,
                 userId = e.userId,
                 nickName = e.nickName()
             )

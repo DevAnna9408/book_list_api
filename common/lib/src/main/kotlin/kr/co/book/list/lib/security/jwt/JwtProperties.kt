d0df0d0aa7cc3c1af45e64961e9b08d363926588
anna <9408commit@gmail.com>
2023-01-14 09:34:21 +0900

    refactor: 자동 로그인 시간을 30일로 설정

--- common/lib/src/main/kotlin/kr/co/book/list/lib/security/jwt/JwtProperties.kt
+++ common/lib/src/main/kotlin/kr/co/book/list/lib/security/jwt/JwtProperties.kt
@@ -8,6 +8,6 @@ import org.springframework.boot.context.properties.ConstructorBinding
 data class JwtProperties (
     val base64EncodedSecret: String,  // JWT 생성/파싱에 사용하는 비밀키
     val tokenDurationHr: Int? = 24,  // 사용자 토큰 유효 시간
-    val swaggerTokenDurationHr: Int? = 120,  // 스웨거 토큰 유효 시간
+    val swaggerTokenDurationHr: Int? = 720,  // 스웨거 토큰 유효 시간
     val generatorEnabled: Boolean = false,  // 스웨거에 토큰 생성기 포함 여부
 )
